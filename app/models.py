b9ded959014976910cfe74b9cb7fd963075c48f5
kotbegimot <iliynaea@gmail.com>
2023-12-16 20:28:47 +0300

    add

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+from app.schemas import StatusEnum
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(150), nullable=False)
+    last_name = Column(String(150), nullable=False)
+    patronymic = Column(String(150), nullable=True)
+    position = Column(String(200), nullable=False)
+
+    tasks = relationship('Task')
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    parent_task = Column(Integer, ForeignKey('tasks.id', ondelete='CASCADE'), nullable=True, index=True)
+    performer = Column(Integer, ForeignKey('employees.id', ondelete='SET NULL'), nullable=True, index=True)
+    deadline = Column(Date, nullable=False)
+    status = Column(ENUM(StatusEnum, name='status_enum', create_type=False), default=StatusEnum.created, nullable=False)
---
+++ app/schemas.py
@@ -0,0 +1,78 @@
+from datetime import date
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class EmployeeBase(BaseModel):
+    patronymic: Optional[str] = None
+
+
+class EmployeeCreate(EmployeeBase):
+    first_name: str
+    last_name: str
+    position: str
+
+
+class EmployeeUpdate(EmployeeBase):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    position: Optional[str] = None
+
+
+class Employee(EmployeeBase):
+    id: int
+    first_name: str
+    last_name: str
+    position: str
+
+    class Config:
+        from_attributes = True
+
+
+class StatusEnum(Enum):
+    created = "created"
+    taken_to_work = 'taken to work'
+    done = "done"
+
+
+class TaskBase(BaseModel):
+    parent_task: Optional[int] = None
+    performer: Optional[int] = None
+
+
+class TaskCreate(TaskBase):
+    name: str
+    deadline: date
+    status: StatusEnum
+
+
+class TaskUpdate(TaskBase):
+    name: Optional[str] = None
+    deadline: Optional[date] = None
+    status: Optional[StatusEnum] = None
+
+
+class Task(TaskBase):
+    id: int
+    name: str
+    deadline: date
+    status: StatusEnum
+
+    class Config:
+        from_attributes = True
+
+
+class BusyEmployee(EmployeeBase):
+    id: int
+    first_name: str
+    last_name: str
+    position: str
+    tasks: list[Task]
+
+
+class MajorTask(BaseModel):
+    name: str
+    deadline: date
+    employees: list[str]
